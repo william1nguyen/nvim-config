9af663b264702e4f14a4c6f9aead01b09a84f68f
william1nguyen <vietanhhd03@gmail.com>
2026-07-22 21:20:16 +0700

    chore: init nvim config (nvchad)

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,21 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "vim",
+    "vimdoc",
+    "go",
+    "gosum",
+    "gomod",
+    "gowork",
+    "gotmpl"
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
